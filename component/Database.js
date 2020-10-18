ddf6da652be4f783924aa8e39382095ccb06f513
Ragi_lts <yuusan1694301@gmail.com>
2020-10-19 02:37:57 +0900

    test06

--- component/Database.js
+++ component/Database.js
@@ -1,8 +1,9 @@
 //ENV LOADING
 require("dotenv").config();
+const pg = require("pg");
+
 exports.pool = function () {
-  const pg = require("pg");
-  const pool = pg.Pool({
+  const pool = new pg.Pool({
     url: process.env.DATABASE_URL,
   });
   pool.query("SELECT * FROM test_table").then((result) => {
