363752c738c01d6847a2c9e0120e7669e0a31468
Ragi_lts <yuusan1694301@gmail.com>
2020-10-19 03:33:51 +0900

    test06

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ bot.on("message", function (event) {
   event
     .reply(event.message.text)
     .then(function (data) {
-      console.log("Success", JSON.stringify(event));
+      console.log("Success", event.source.userId, event.message.text);
     })
     .catch(function (error) {
       console.log("Error", error);
