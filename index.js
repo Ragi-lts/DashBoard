bb03edfa033c87a59dddd8d353f4de429db1d16c
Ragi_lts <yuusan1694301@gmail.com>
2020-10-20 23:49:37 +0900

    module_Change001

--- index.js
+++ index.js
@@ -1,55 +1,38 @@
+"use strict";
 //ENV LOADING
-require("dotenv").config();
-
-const {LineClient} = require('messaging-api-line')
+const line = require("@line/bot-sdk");
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const Zoom = require("./component/Zoom");
 const db = require("./component/Database");
 const inform = require("./component/Inform");
 
-const client = new LineClient({
-  accessToken: process.env.CHANNEL_ACCESS_TOKEN,
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
+};
+const client = new line.Client(config);
+
+app.post("/linewebhook", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
 });
 
 //channelId: process.env.CHANNEL_ID,
-
-const parser = bodyParser.json({
-  verify: function (req, res, buf, encoding) {
-    req.rawBody = buf.toString(encoding);
-  },
-});
-
-app.post("/linewebhook", parser, function (req, res) {
-  if (!bot.verify(req.rawBody, req.get("X-Line-Signature"))) {
-    return res.sendStatus(400);
+function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
   }
-  bot.parse(req.body);
-  return res.json({});
-});
 
-bot.on("message", function (event) {
-  event
-    .reply(event.message.text)
-    .then(function (data) {
-      console.log("Success", event.source.userId, event.message.text);
-    })
-    .catch(function (error) {
-      console.log("Error", error);
-    });
-  bot.getUserProfile(event.source.userId).then((profile) => {
-    let data = inform.FinishResister(
-      profile.displayName,
-      "TEST",
-      "11/21",
-      "19:00",
-      new Date().toString
-    );
-    event.reply(data);
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: event.message.text,
   });
-});
+}
 
 app.listen(process.env.PORT || 80, function () {
   console.log("LineBot is runnning.");
